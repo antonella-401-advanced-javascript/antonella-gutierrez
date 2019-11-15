cb69e999d7e111cb9228e4f252b8a09677a03a71
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-11-15 10:18:24 -0800

    under construction temp display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import styles from './App.css';
 
 const App = () => (
   <>
+    <div className={styles.Construction}>
+      <img src={'./src/underconstruction.jpg'} />
+    </div>
   </>
 );
 
