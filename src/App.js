71079c054f146f16d7d49489a897fd42aa81bd06
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-11-15 21:13:48 -0800

    changed image source

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import styles from './App.css';
 const App = () => (
   <>
     <div className={styles.Construction}>
-      <img src={'./src/underconstruction.jpg'} />
+      <img src={'https://cdn.pixabay.com/photo/2019/02/20/15/21/underconstruction-4009361_1280.jpg'} />
     </div>
   </>
 );
