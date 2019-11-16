ec799adbd1827bb361beff9a7efda1cf67de432f
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-11-15 21:19:44 -0800

    minor change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import HomeContainer from './containers/homeContainer/Home';
+import AboutContainer from './containers/aboutContainer/About';
+import ContactContainer from './containers/contactContainer/Contact';
 
 const App = () => {
   return (
@@ -10,8 +12,8 @@ const App = () => {
         <Header />
         <Switch>
           <Route exact path="/" component={HomeContainer} />
-          <Route path="/about" />
-          <Route path="/contact" />
+          <Route path="/about" component={AboutContainer} />
+          <Route path="/contact" component={ContactContainer} />
         </Switch>
       </>
     </Router>
