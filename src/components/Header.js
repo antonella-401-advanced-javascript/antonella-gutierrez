1ca7773bca54dbef46b937751b6d0e0c55818bed
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-11-21 17:43:43 -0800

    modified background and Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Header.css';
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.Header}>
       <h1>Antonella Gutierrez</h1>
       <nav>
         <Link to="/">Home</Link>
