dd79a61fea0468f6aca051f0a9b5a582d8f4f276
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-12-03 14:39:14 -0800

    adjustments to deploy to netlify

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './About.css';
+import profilePic from '../../Anty.jpg';
 
 const About = () => {
   return (
     <div className={styles.About}>
       <section>
-        <img src="./src/Anty.jpg"/>
+        <img src={profilePic}/>
         <div>
           <p>
           I am a Software Engineer based in Portland, Oregon. Specialized in the implementation of JavaScript and MERN stack to build well rounded applications. I am eager about furthering my knowledge of technology and gaining more experience with developing software that can solve real-world problems.
