6533748c23e878d72d101fdf5e2740fdb4c0b982
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-12-03 15:55:08 -0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
         test: /.(jpeg|jpg|png|svg)$/,
         use: {
           loader: 'url-loader',
-          options: { limit: 1000 },
+          options: { limit: 1000000 },
         },
       },
       {
