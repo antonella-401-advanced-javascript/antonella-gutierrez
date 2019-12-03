b44a3c45c3d4e3242837c2cce9ea562dfb8d25ec
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-12-03 15:24:43 -0800

    .

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './About.css';
+import Anty from '../../Anty.jpg';
 
 const About = () => {
-  const profilePic = '../../Anty.jpg';
   return (
     <div className={styles.About}>
       <section>
-        <img src={profilePic}/>
+        <img src={Anty}/>
         <div>
           <p>
           I am a Software Engineer based in Portland, Oregon. Specialized in the implementation of JavaScript and MERN stack to build well rounded applications. I am eager about furthering my knowledge of technology and gaining more experience with developing software that can solve real-world problems.
