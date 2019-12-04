da58138056fa94d4c0fc8b11deeb144a47ce825d
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-12-04 13:00:18 -0800

    working on projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import bashful from './BASHful-logo.png';
+import tarot from './tarot.png';
+import poke from './pokebowl.png';
 
 const Projects = () => {
+  return (
+    <div>
 
+      <a>
+        <img src={bashful} />
+      </a>
+
+      <a>
+        <img src={tarot} />
+      </a>
+
+      <a>
+        <img src={poke} />
+      </a>
+
+    </div>
+  );
 };
 
 export default Projects;
