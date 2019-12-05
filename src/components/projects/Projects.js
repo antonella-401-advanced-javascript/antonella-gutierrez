01d68eb4e110af66df4a015e10380c3991c798f7
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-12-04 21:22:59 -0800

    working on project component

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,23 +2,37 @@ import React from 'react';
 import bashful from './BASHful-logo.png';
 import tarot from './tarot.png';
 import poke from './pokebowl.png';
+import styles from './Projects.css';
 
 const Projects = () => {
   return (
-    <div>
+    <div className={styles.Projects}>
+      <section>
+        <a>
+          <img src={bashful} />
+          <p>
+            I will be writing a description about what Bashful does and the tech stack used.
+          </p>
+        </a>
+      </section>
 
-      <a>
-        <img src={bashful} />
-      </a>
+      <section>
+        <a>
+          <img src={tarot} />
+          <p>
+            I will be writing a description about what Tarot does and the tech stack used.
+          </p>
+        </a>
+      </section>
 
-      <a>
-        <img src={tarot} />
-      </a>
-
-      <a>
-        <img src={poke} />
-      </a>
-
+      <section>
+        <a>
+          <img src={poke} />
+          <p>
+            I will be writing a description about what Pokebowl does and the tech stack used.
+          </p>
+        </a>
+      </section>
     </div>
   );
 };
